d56a07f922d23658cb3d3146532d1c93401e8654
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 22:48:22 +0100

    feat: implement tf-atom-sqs-queue-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "arn" {
+  description = "ARN of the SQS queue"
+  value       = try(aws_sqs_queue.this[0].arn, null)
+}
+
+output "id" {
+  description = "URL of the SQS queue"
+  value       = try(aws_sqs_queue.this[0].id, null)
+}
+
+output "url" {
+  description = "URL of the SQS queue"
+  value       = try(aws_sqs_queue.this[0].url, null)
+}
+
+output "name" {
+  description = "Name of the SQS queue"
+  value       = try(aws_sqs_queue.this[0].name, null)
 }
